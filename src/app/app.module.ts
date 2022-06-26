ae46f180eb1f0c783a80d7622960ed4da219c8e6
Coen Mooij <coenmooij@gmail.com>
2022-06-26 16:48:38 +0200

    Remove generated code leftover to fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core';
 import { RoutingModule } from './routing/routing.module';
-import { TestpietjeComponent } from './testpietje/testpietje.component';
 
 @NgModule({
-    declarations: [AppComponent, TestpietjeComponent],
+    declarations: [AppComponent],
     imports: [BrowserModule, RoutingModule, RouterModule, CoreModule],
     bootstrap: [AppComponent],
 })
